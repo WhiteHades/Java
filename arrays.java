f443d7671645c04da9d3f55544c9c16322afe46a
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-17 18:21:45 +0600

    arrays

--- arrays.java
+++ arrays.java
@@ -1,5 +1,11 @@
 public class arrays {
     public static void main(String args[]) {
+        // finding sum of all elements
+        int A[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum = sum + A[i];
+        }
 
     }
 }
