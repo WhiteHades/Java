945dff72f11a8fd07272c1b8bdd8a1d7bcb2194f
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-18 15:36:31 +0600

    updated name of class.java

--- writingclass.java
+++ writingclass.java
@@ -1,50 +1,43 @@
-public class class
-{
-    public static void main(String args[])
-    {
+public class writingclass {
+    public static void main(String args[]) {
         class circle {
             public double radius;
-        
+
             // whenever there's computation involved
             // it's a method
             public double area() {
                 return 4.4; // example or else error if nothing given
             }
-        
+
             public double perimeter() {
                 return 4.4; // example or else error if nothing given
             }
         }
-        
+
         class rectangle {
             public int length;
             public int breadth;
-        
-            public int area()
-            {
+
+            public int area() {
                 return 4;
             }
-        
-            public int perimeter()
-            {
+
+            public int perimeter() {
                 return 4;
             }
         }
 
-        class student
-        {
+        class student {
             public int roll;
             public String name;
             public String course;
             public int m1, m2, m3;
 
-            public int total()
-            {
+            public int total() {
                 return 4;
             }
 
-            public float average()
-            {
+            public float average() {
                 return 4;
             }
         }
