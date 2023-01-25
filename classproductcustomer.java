339858165916acc7822216f04b0966fc8183931d
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-25 20:28:03 +0600

    customer update

--- classproductcustomer.java
+++ classproductcustomer.java
@@ -26,12 +26,48 @@ class Product {
         }
     }
 
-    public void setQty(double q) {
+    public void setQty(short q) {
         if (q >= 0) {
             qty = q;
         }
     }
 
     // constructor
+    public Product(String i, String n, double p, short qty) {
+        itemno = i;
+        name = n;
+        setPrice(p);
+        setQty(qty);
+    }
+}
 
+class Customer {
+    private String cusid;
+    private String name;
+    private String address;
+    private String phno;
+
+    public String getCusid() {
+        return cusid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String a) {
+        address = a;
+    }
+
+    public void setPhone(String p) {
+        phno = p;
+    }
+
+    // Constructors
+    public Customer(String id, String n, String ad, String num) {
+        cusid = id;
+        name = n;
+        setAddress(ad);
+        setPhone(num);
+    }
 }
