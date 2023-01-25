12bf9514083816bb8500917902e09108a9d4739a
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-25 18:21:13 +0600

    proerties

---
+++ properties.java
@@ -0,0 +1,16 @@
+class Rectangle {
+    private double length;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double l) {
+        length = 1;
+    }
+
+}
+
+public class properties {
+
+}
