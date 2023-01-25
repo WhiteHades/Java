0fa386af37c27f11288d8f5c16734affc5896f12
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-25 19:04:47 +0600

    constructors practise

--- constructors.java
+++ constructors.java
@@ -19,6 +19,57 @@ class Rectangle {
     }
 }
 
+class Cylinder {
+    private int radius;
+    private int height;
+
+    // read
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    // write
+    public void setRadius(int r) {
+        if (r >= 0) {
+            radius = r;
+        }
+    }
+
+    public void setHeight(int h) {
+        if (h >= 0) {
+            height = h;
+        }
+    }
+
+    public void setDimentions(int r, int h) {
+        if (r >= 0 && h >= 0) {
+            radius = r;
+            height = h;
+        }
+    }
+
+    // constructors
+    public Cylinder() {
+        radius = 0;
+        height = 0;
+    }
+
+    public Cylinder(int r) {
+        radius = r;
+        height = 1;
+    }
+
+    public Cylinder(int r, int h) {
+        radius = r;
+        height = h;
+    }
+
+}
+
 public class constructors {
     public static void main(String[] args) {
 
