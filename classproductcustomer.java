f3d5503c8a5ba84ef597f7af1eee9022fd62e3e4
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-25 19:29:35 +0600

    constructor practise

---
+++ classproductcustomer.java
@@ -0,0 +1,23 @@
+class Product {
+    private String itemno;
+    private String name;
+    private double price;
+    private short qty;
+
+    public String getItemno() {
+        return itemno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public short getQtr() {
+        return qty;
+    }
+
+}
