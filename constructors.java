c944974c44c2ebda00c58c9588165fdcb1c86255
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-25 18:45:55 +0600

    constructors

---
+++ constructors.java
@@ -0,0 +1,24 @@
+class Rectangle {
+    private double length;
+    private double breadth;
+
+    // constructor
+    public Rectangle() {
+        length = 1;
+        breadth = 1;
+    }
+
+    public Rectangle(double l, double b) {
+        length = l;
+        breadth = b;
+
+    }
+
+    public Rectangle(double s) {
+        length = breadth = s;
+    }
+}
+
+public class constructors {
+
+}
