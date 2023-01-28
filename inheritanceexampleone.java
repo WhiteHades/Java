a63be8ce5aee4e968aa73eb4ffc1aa17a6ad1e57
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-01-28 21:06:25 +0600

    update

--- inheritanceexampleone.java
+++ inheritanceexampleone.java
@@ -1,3 +1,13 @@
+class Parent{
+    System.out.println("Parent Constructor");
+}
+
+class Child extends Parent {
+    public Child() {
+        System.out.println("Child Constructor");
+    }
+}
+
 public class inheritanceexampleone {
     public static void main(String args[]) {
 
