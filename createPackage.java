2e06e0209b44798b43a8668a86dea3df71560f18
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-02-04 23:55:51 +0600

    update

---
+++ createPackage.java
@@ -0,0 +1,15 @@
+package mypackage;
+package mypackage.inner;
+// // Creating package
+
+// include in the very first line
+// package yourpackagename;
+
+// //then in commandline
+// javac -d . Demo.java
+
+public class createPackage {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ packagesUse.java
@@ -0,0 +1,9 @@
+import java.lang.Runnable;
+//import java.lang.String;
+
+public class packagesUse {
+    public static void main(String[] args) {
+        java.lang.String str;
+        str = new java.lang.String("abc");
+    }
+}
