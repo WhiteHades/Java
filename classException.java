69cd3736d2aa01f0bcddb3dce6adab18c8efc73e
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-02-12 00:27:48 +0600

    checked and unchecked exceptions

---
+++ classException.java
@@ -0,0 +1,13 @@
+public class classException {
+    static void fun1() {
+        System.out.println(10 / 0);
+    }
+
+    static void fun2() {
+        System.out.println();
+    }
+
+    static void fun3() {
+
+    }
+}
