ed91d925440f1f622508e31627fac38ffd6aa6bc
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-02-11 23:17:30 +0600

    exceptionHandling

---
+++ exceptionHandling.java
@@ -0,0 +1,17 @@
+public class exceptionHandling {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        try {
+            a = 10;
+            b = 0;
+            c = a / b;
+
+            System.out.println("Result is " + c);
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("Division by zero is not possible." + "\n" + e);
+        }
+    }
+}
---
+++ exceptionHandlingNested.java
@@ -0,0 +1,21 @@
+public class exceptionHandlingNested {
+    public static void main(String[] args) {
+        try {
+            int a[] = { 10, 0, 8, 3, 5 };
+
+            try {
+                int r = a[0] / a[1];
+                System.out.println(r);
+            } catch (ArithmeticException e) {
+                System.out.println(e);
+            }
+            System.out.println(a[10]);
+
+        }
+
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println(e);
+        }
+
+    }
+}
