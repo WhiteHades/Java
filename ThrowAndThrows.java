06b1836adf996d510de6c21f0821b3da82345821
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-02-12 19:00:39 +0600

    throws exception

--- ThrowAndThrows.java
+++ ThrowAndThrows.java
@@ -11,7 +11,28 @@ public class ThrowAndThrows {
         return meth2();
     }
 
-    public static void main(String[] args) {
-        meth3();
+    static int area(int l, int b) throws Exception {
+        if (l < 0 || b < 0) {
+            throw new Exception();
+        }
+        return l * b;
+    }
+
+    static void meth4() throws Exception {
+        System.out.println("Area is " + area(-10, 5));
+    }
+
+    public static void main(String[] args) throws Exception {
+        // meth3();
+
+        // you could use this try catch in meth2 and meth1 as well
+        try {
+            meth3();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        meth4();
+
     }
 }
